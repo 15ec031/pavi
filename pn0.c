a974bb861b12e574615fe0d626a50bf776285eda
15ec031 <pavithravimala19@gmail.com>
2016-09-22 14:13:43 +0530

    Create pn0.c

---
+++ pn0.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("\nEnter the number");
+scanf("%d",&n);
+if(n>0)
+{
+printf("%d is positive number",n);
+}
+if else(n<0)
+{
+printf("%d is negative number",n);
+}
+if else(n=0)
+{
+printf("%d is zero",n);
+}
+return 0;
+}
