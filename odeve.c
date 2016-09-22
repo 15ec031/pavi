f6e4b9e2565f3b73be02cbb05936c7f796a6881d
15ec031 <pavithravimala19@gmail.com>
2016-09-22 13:59:29 +0530

    Create odeve.c

---
+++ odeve.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("enter the number");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("%d is even number",n);
+}
+if else
+{
+printf("%d is odd number",n);
+}
+return 0;
+}
